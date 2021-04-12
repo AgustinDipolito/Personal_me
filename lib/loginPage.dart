8ad265505fbfb94fb4fd4b868b252417336c2fe4
YisusGo <agusdipo04@gmail.com>
2021-04-12 17:05:41 -0300

    script para circulos,sin optimizar

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -22,12 +22,31 @@ class _LoginPageState extends State<LoginPage> {
             child: Align(
               alignment: Alignment.center,
               child: Container(
-                height: screenSize.height * .8,
+                height: screenSize.height * .77,
                 width: screenSize.width,
                 decoration: BoxDecoration(color: kPrimaryColor),
               ),
             ),
           ),
+          Positioned(
+              top:
+                  0, //(screenSize.height / 2 - ((screenSize.height * 0.7) / 2)),
+              left: 0, // (screenSize.width * 0.25) - screenSize.width * 0.25,
+              child: Container(
+                height: screenSize.width * 0.5,
+                width: screenSize.width * 0.5,
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle, color: kPrimaryColor),
+              )),
+          Positioned(
+              top: 0,
+              left: screenSize.width / 2,
+              child: Container(
+                height: screenSize.width * 0.5,
+                width: screenSize.width * 0.5,
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle, color: kSecondaryColor),
+              ))
         ],
       ),
     );
