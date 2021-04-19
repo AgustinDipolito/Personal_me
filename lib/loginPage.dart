5c5c6e9520888ccd51e01b28526beae072c16c2a
YisusGo <agusdipo04@gmail.com>
2021-04-18 22:26:41 -0300

    added login button

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -14,89 +14,104 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double mitadW = screenSize.width / 2;
+    // ignore: unused_local_variable
     double mitadH = screenSize.height / 2;
     //SystemChrome.setEnabledSystemUIOverlays([]);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: kSecondaryColor,
-      body: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: screenSize.height * .77,
-                width: screenSize.width,
-                decoration: BoxDecoration(color: kPrimaryColor),
-              ),
+      body: Stack(children: <Widget>[
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: screenSize.height * .77,
+              width: screenSize.width,
+              decoration: BoxDecoration(color: kPrimaryColor),
             ),
           ),
-          //seccion de circulares
-          positionedCircle(screenSize, 0, 0, kPrimaryColor, mitadW),
-          positionedCircle(screenSize, mitadW, 0, kSecondaryColor, mitadW),
-          positionedCircle(screenSize, mitadW, (screenSize.height * .77),
-              kPrimaryColor, mitadW),
-          positionedCircle(
-              screenSize,
-              mitadW + mitadW / 4,
-              screenSize.height * .77 + mitadW / 4,
-              kSecondaryColor,
-              mitadW / 2),
-          Positioned(
-              top: screenSize.height * .6,
-              left: 0,
-              child: Container(
-                height: screenSize.height * .4,
-                width: mitadW,
-                decoration: BoxDecoration(
-                  color: kSecondaryColor,
-                  borderRadius: BorderRadius.circular(mitadW),
-                ),
-              )),
-          Positioned(
-              child: Container(
-                  padding: EdgeInsets.only(left: mitadW / 4, right: mitadW / 4),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(height: mitadW),
-                      Text(
-                        "Personal",
-                        style: TextStyle(
+        ),
+        //seccion de circulares
+        positionedCircle(screenSize, 0, 0, kPrimaryColor, mitadW),
+        positionedCircle(screenSize, mitadW, 0, kSecondaryColor, mitadW),
+        positionedCircle(screenSize, mitadW, (screenSize.height * .77),
+            kPrimaryColor, mitadW),
+        positionedCircle(screenSize, mitadW + mitadW / 4,
+            screenSize.height * .77 + mitadW / 4, kSecondaryColor, mitadW / 2),
+        Positioned(
+            top: screenSize.height * .6,
+            left: 0,
+            child: Container(
+              height: screenSize.height * .4,
+              width: mitadW,
+              decoration: BoxDecoration(
+                color: kSecondaryColor,
+                borderRadius: BorderRadius.circular(mitadW),
+              ),
+            )),
+        Positioned(
+            child: Container(
+                padding: EdgeInsets.only(left: mitadW / 4, right: mitadW / 4),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: mitadW),
+                    Text(
+                      "Personal",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: kSecondaryColor,
+                        fontSize: mitadW / 4,
+                      ),
+                    ),
+                    Text(
+                      "Me",
+                      style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: kSecondaryColor,
-                          fontSize: mitadW / 4,
-                        ),
-                      ),
-                      Text(
-                        "Me",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: kSecondaryColor,
-                            fontSize: mitadW / 4),
-                      ),
-                      // SizedBox(height: mitadW * 1.5),
-                      TextField(
-                          decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: kSecondaryColor),
-                        enabledBorder: new UnderlineInputBorder(
-                            borderSide: new BorderSide(color: kSecondaryColor)),
-                      )),
-                      TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            labelStyle: TextStyle(color: kSecondaryColor),
-                            enabledBorder: new UnderlineInputBorder(
-                                borderSide:
-                                    new BorderSide(color: kSecondaryColor)),
-                          )),
-                    ],
-                  ))),
-        ],
-      ),
+                          fontSize: mitadW / 4),
+                    ),
+                    // SizedBox(height: mitadW * 1.5),
+                    TextField(
+                        decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: kSecondaryColor),
+                      enabledBorder: new UnderlineInputBorder(
+                          borderSide: new BorderSide(color: kSecondaryColor)),
+                    )),
+                    TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: kSecondaryColor),
+                          enabledBorder: new UnderlineInputBorder(
+                              borderSide:
+                                  new BorderSide(color: kSecondaryColor)),
+                        )),
+                    Row(
+                      children: <Widget>[
+                        Expanded(child: SizedBox()),
+                        ElevatedButton.icon(
+                            onPressed: () {
+                              print("je");
+                            },
+                            label: Text("Login",
+                                style: TextStyle(color: kPrimaryColor)),
+                            icon: Icon(
+                              Icons.forward,
+                              color: kPrimaryColor,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: kSecondaryColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(20),
+                                ))),
+                      ],
+                    )
+                  ],
+                ))),
+      ]),
     );
   }
 
